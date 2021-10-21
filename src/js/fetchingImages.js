ccacb0b85070641b712efb6a35a77d9cd6007a4f
Viktor Kostiuchenko <kostiuchenkoviktor@gmail.com>
2021-10-21 23:23:05 +0300

    Integrate async function

--- src/js/fetchingImages.js
+++ src/js/fetchingImages.js
@@ -2,23 +2,24 @@ export class Pixabay {
     constructor() {
       this.imagesQuery = '';
       this.page = 1;       
-    }
-    fetchImages() {
+  }
+  
+    async fetchImages() {
       const BASE_URL = 'https://pixabay.com/api/'
       const KEY = '23833327-aee66bbf86a23c3fb1d188dcb'
       let url = `${BASE_URL}?key=${KEY}&q=${this.imagesQuery}&image_type=photo&orientation=horizontal&per_page=12&page=${this.page}`
 
       this.updatePage()
-      return fetch(url)
-        .then(response => {
-          if (response.ok) {
-            return response.json()
-          }
-          throw new Error (response.statusText)
-        })
-        .then(obj => obj.hits)
-        .catch(error => alert(error))
-    }
+
+      try {
+        const response = await fetch(url)
+        const newImages = await response.json()
+        return newImages.hits
+      } catch (error) {
+        alert(`Query failed ${error}`)
+      }
+  }
+  
     updatePage(){
       this.page +=1
     }
